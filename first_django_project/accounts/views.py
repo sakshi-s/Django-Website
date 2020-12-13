7e3da728dd934001554e574b4e015a98f3f7b167
Sakshi <sakshis.1407@gmail.com>
2020-12-13 17:15:03 +0530

    implement user login

--- first_django_project/accounts/views.py
+++ first_django_project/accounts/views.py
@@ -4,6 +4,23 @@ from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
+def login(request):
+    if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request,'login.html') 
+
 def register(request):
 
     if( request.method == 'POST') :
